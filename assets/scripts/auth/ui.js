19ce43b3af049b3fc12154aee3d1b283898c26d4
jwaskow <thewasko@gmail.com>
2017-02-17 11:57:50 -0500

    Remove useless code from ui.js
    - Leftover code from tic-tac-toe sign in/out success
    - added status box to html

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -34,38 +34,21 @@ const failure = () => {
 const signInSuccess = () => {
   $('#status-box').text('Sign In Successful');
 
-  $('.create-board-button').removeClass('hidden');
-  $('#get-index').removeClass('hidden');
-  $('#showGameById').removeClass('hidden');
   $('#signUpButton').addClass('hidden');
   $('#signInButton').addClass('hidden');
   $('#changePassButton').removeClass('hidden');
   $('#sign-out').removeClass('hidden');
-  $('#declare-winner').removeClass('hidden');
-  $('#already-won').removeClass('hidden');
-  $('#total-games-played').removeClass('hidden');
-  $('#show-total-games').removeClass('hidden');
   $('#signInModal').modal('hide');
 };
 
 // On sign out, hide game board elements
 const signOutSuccess = () => {
-  $('#status-box').text('Sign Out Successful.  Please Sign Up or Sign In to play again.');
+  $('#status-box').text('Sign Out Successful.  Please Sign Up or Sign In.');
 
-  $('.create-board-button').addClass('hidden');
-  $('.container-grid').addClass('hidden');
-  $('#get-index').addClass('hidden');
-  $('#showGameById').addClass('hidden');
   $('#signUpButton').removeClass('hidden');
   $('#signInButton').removeClass('hidden');
   $('#changePassButton').addClass('hidden');
   $('#sign-out').addClass('hidden');
-  $('#declare-winner').addClass('hidden');
-  $('#declare-winner').text('');
-  $('#already-won').addClass('hidden');
-  $('#already-won').text('');
-  $('#total-games-played').addClass('hidden');
-  $('#show-total-games').addClass('hidden');
 };
 
 module.exports = {
